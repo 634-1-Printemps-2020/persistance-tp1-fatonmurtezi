7fc4e70e713282b191b7c4fa66ab4b2e669b861c
Faton Murtezi <faton.murtezi@hes-so.ch>
2020-02-23 17:01:21 +0100

    counter

--- src/main/java/ch.hesge.cours634.counter/Counter.java
+++ src/main/java/ch.hesge.cours634.counter/Counter.java
@@ -1,5 +1,28 @@
 package ch.hesge.cours634.counter;
 
 public class Counter implements ICounter {
+    private int value;
 
+    public Counter() {
+        this.value = 0;
+    }
+
+    public Counter( int value){
+        this.value = value;
+    }
+
+    @Override
+    public void inc() throws CounterException {
+        this.value = this.value + 1;
+    }
+
+    @Override
+    public void add(int step) throws CounterException {
+
+    }
+
+    @Override
+    public int getValue() {
+        return 0;
+    }
 }
